20c05bc91701daeceb4c11f76666e22a3d21b180
Carlos Morán <moran_krlos@hotmail.com>
2020-07-17 17:54:10 -0600

    creating movie model and migration

---
+++ migrations/20200717234721-create-movie.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      rentalPrice: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      salePrice: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      availability: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Movies');
+  }
+};
---
+++ models/movie.js
@@ -0,0 +1,27 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Movie extends Model {
+  static associate(models) {
+    // define association here
+  }
+};
+Movie.init({
+  title: DataTypes.STRING,
+  description: DataTypes.STRING,
+  image: {
+    type: DataTypes.STRING,
+    validate: {
+      isUrl: true
+    },
+  },
+  rentalPrice: DataTypes.FLOAT,
+  salePrice: DataTypes.FLOAT,
+  availability: DataTypes.BOOLEAN
+}, {
+  sequelize,
+  modelName: 'Movie',
+});
+
+module.exports = Movie;
