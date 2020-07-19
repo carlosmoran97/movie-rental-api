62c183f228904d78546ca80f1504b05df19a904c
Carlos Morán <moran_krlos@hotmail.com>
2020-07-19 02:33:58 -0600

    allow not authenticated users searching movies

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,11 +1,17 @@
 const jwt = require('express-jwt');
 const secret = process.env.SECRET;
 
-// ===========================================================================
-// Authenticate attach user to request object, but doesn't care about the role
-// ===========================================================================
+// ============================================================================
+// Authenticate: attach user to request object, but doesn't care about the role
+// can be anonymous, but if user provide token then authenticate.
+// If you require user role then use authorize.
+// ============================================================================
 function authenticate(){
-    return jwt({ secret, algorithms: ['HS256'] });
+    return jwt({
+        secret,
+        algorithms: ['HS256'],
+        credentialsRequired: false
+    });
 }
 
 module.exports = authenticate;
