259483d606a4b467171f0e9907a1fd172a8386fb
Carlos Morán <moran_krlos@hotmail.com>
2020-07-19 21:02:31 -0600

    authorization header can be undefined on validate token method

--- helpers/validate-token.js
+++ helpers/validate-token.js
@@ -1,6 +1,9 @@
 const redis = require('../config/redis');
 
 module.exports = async (req, res, next) => {
+    if(!req.headers.authorization){
+        return next();
+    }
     const token = req.headers.authorization.split(' ')[1] || req.params.token;
     if (!token) {
         return next();
